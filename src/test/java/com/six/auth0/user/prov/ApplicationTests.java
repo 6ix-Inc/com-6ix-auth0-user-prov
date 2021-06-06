fff3cd6e2480f4e58f954f577b1f60fd4f615679
Harjinder Dhanjal <malotian@gmail.com>
2021-06-06 22:43:19 +0530

    mvn -> gradle

---
+++ src/test/java/com/six/auth0/user/prov/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.six.auth0.user.prov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
